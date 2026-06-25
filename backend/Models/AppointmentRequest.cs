331438b18d6103c4d6c9a90cf5137e7b40af87c9
yaman <yamanabdalmalek@gmail.com>
2026-06-25 17:16:18 +0300

    Creating Models and Initializing DB

---
+++ backend/Models/AppointmentRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetHaven.Models
+{
+    public class AppointmentRequest
+    {
+        [Key]
+        public int AppointmentRequestId { get; set; }
+
+        [MaxLength(500)]
+        public string? ImageURL { get; set; }
+
+        [MaxLength(50)]
+        public string? HealthStatus { get; set; }
+
+        public string? Notes { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
